72e75c481b83d080b1327a8aa08dedab846b5d43
YeshwanthDandu180903 <yeshwanthdandu2003@gmail.com>
2025-09-30 14:03:46 +0530

    Sample Data preparation is completed

---
+++ my_code.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+data={'Name':['Yeshwanth','Lohith','Arun','Nikhith'],'Age':[21,22,20,21],'City':['Hyderabad','Kerala','Pondicherry','Shanghai']}
+
+df=pd.DataFrame(data)
+
+
+#creating a data directory for storing the data
+data_dir='data'
+os.makedirs(data_dir,exist_ok=True)
+
+#defining the path
+file_path=os.path.join(data_dir,'sample_data.csv')
+
+#save the dataframe to a csv file,including column names
+df.to_csv(file_path,index=False)
+
+print(f"CSV file saved to {file_path}")
